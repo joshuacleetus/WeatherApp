70f7a9ab9218f009cc5fb829b960422eb2233308
Joshua Cleetus <joshua.cleetus@gmail.com>
2025-05-15 08:52:06 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Joshua Cleetus on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
